9cd7ea98a0b79433a2c9fae181548e871575e857
CedricLahaye <cedric.lahaye@my-digital-school.org>
2023-04-13 15:27:24 +0200

    Ajoute compteur clique avatar

--- frontend/src/components/Profile/Avatar.jsx
+++ frontend/src/components/Profile/Avatar.jsx
@@ -1,10 +1,37 @@
+import { useRef } from 'react'
 import '../../styles/Avatar.scss'
 
 const Avatar = ({ avatar }) => {
+  const image = useRef(null)
+  // Compteur de clique
+  let counter = 0
+
+  // Gestion du clique
+  const handlePress = () => {
+    counter++
+    // Si le nombre de clique atteint 5, surprise
+    if (counter >= 5) {
+      // l'image rotationne
+      console.log('wouwh tu cliques vite')
+
+      image.current.classList.add('animation')
+      setTimeout(() => {
+        if (image && image.current) {
+          image.current.classList.remove('animation')
+        }
+      }, 5000)
+    }
+    setTimeout(() => {
+      counter = 0
+    }, 3000)
+  }
+
   return (
     <img
+      ref={image}
       className='avatar'
       src={`${process.env.REACT_APP_IMAGES_URL}${avatar.url}`}
+      onClick={handlePress}
     />
   )
 }
