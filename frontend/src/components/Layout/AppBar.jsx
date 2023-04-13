0368776327c897b02f4af352a53efb9d7c7ebb44
CedricLahaye <cedric.lahaye@my-digital-school.org>
2023-04-13 16:32:32 +0200

    modifie == en ===

--- frontend/src/components/Layout/AppBar.jsx
+++ frontend/src/components/Layout/AppBar.jsx
@@ -87,10 +87,10 @@ function ResponsiveAppBar () {
 
   const handleCloseUserMenu = (setting) => {
     // Lors du clique sur le paramètre Profile
-    if (setting == "Profile") {
+    if (setting === 'Profile') {
       // Redirection sur la page profile de l'utilisateur connecter
       // Modifier lorsque la connexion sera fonctionelle
-      navigate("/users/1")
+      navigate('/users/1')
     }
     setAnchorElUser(null)
   }
