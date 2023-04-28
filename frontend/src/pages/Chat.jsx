d954050ff0cff4dbdbabb9a1e7035e1a091e1320
kalvin <kalvin.daniel56@laposte.net>
2023-04-28 09:37:42 +0200

    feat : add messages socket

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -3,14 +3,28 @@ import ChatSendingForm from '../components/Chat/ChatSendingForm'
 import { GET_CHAT_MESSAGE } from '../graphql/queries/chatsQueries'
 import { useParams } from 'react-router-dom'
 import {ChatBubble} from '../components/Chat/ChatBubble'
+import { useEffect, useState } from 'react'
+import { subscribeToMessages } from '../services/socket'
 
 const Chat = () => {
   const { id } = useParams()
-  const { loading, error, data } = useQuery(GET_CHAT_MESSAGE(id))
+  const getMessages = useQuery(GET_CHAT_MESSAGE(id))
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    if (getMessages.data) {
+      setMessages(getMessages.data.messages.data);
+    }
+  }, [getMessages]);
+
+  useEffect(() => {
+    subscribeToMessages(setMessages);
+  }, []);
+
   return (
     <>
       <h1>Chat</h1>
-      {data?.messages?.data.map((message) => (
+      {messages.map((message) => (
         <ChatBubble
           key={message.id}
           content={message?.attributes?.messageText}
--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -45,4 +45,56 @@ export const subscribeToPosts = (setPosts) => {
   };
 };
 
+export const subscribeToMessages = (setMessages) => {
+  // Listening to socket events for new posts
+  socket.on("connect", () => {
+    console.log("Socket connected");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected");
+  });
+
+  socket.on("message:create", (data) => {
+    console.log("New message received", data);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        id: String(data.id),
+        attributes: {
+          ...data,
+          users_permissions_user: {
+            id : data.updatedBy.id,
+            data : data.updatedBy
+          }
+        }
+      },
+    ]);
+  });
+
+  // receive new messages sent by the server
+  socket.on("message:update", (data) => {
+    console.log("Message updated", data);
+    setMessages((prevMessages) => {
+      const index = prevMessages.findIndex(
+        (message) => Number(message.id) === Number(data.id)
+      );
+      prevMessages.splice(index, 1);
+      return prevMessages;
+    });
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { id: String(data.id), attributes: data },
+    ]);
+  });
+
+  return () => {
+    socket.off("connect");
+    socket.off("disconnect");
+    socket.off("message");
+    socket.off("message:create");
+    socket.off("message:update");
+  };
+};
+
 export default socket;
