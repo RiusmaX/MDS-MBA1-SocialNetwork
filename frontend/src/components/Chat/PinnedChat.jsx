7b4c130de91d94e47ea3fb02f3e1e0b3d035b2f7
melim <lvsqmelanie@gmail.com>
2023-05-24 10:41:29 +0200

    feat: begin pinned/sticky chat view

---
+++ frontend/src/components/Chat/PinnedChat.jsx
@@ -0,0 +1,13 @@
+
+import Chat from '../pages/Chat'
+import '../../styles/PinnedChat.scss'
+
+const PinnedChat = (chatId) => {
+  return (
+    <div id='pinnedchat'>
+      <Chat />
+    </div>
+  )
+}
+
+export default PinnedChat
