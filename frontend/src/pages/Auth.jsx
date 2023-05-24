d988259a5affdbf208a1191f3b0b37d365ff5092
Samuel Cottarel <samcot97@gmail.com>
2023-05-24 11:40:56 +0200

    fix: authent

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import '../styles/Auth.scss'
 import '../styles/Global.scss'
 import InputField from '../components/Global/Input/InputField'
 import { FaUser, FaLock } from 'react-icons/fa'
 import Julo from '../assets/images/Julo.png'
+import juloError from '../assets/images/Julo_error.png'
 import SubmitButton from '../components/Global/Buttons/SubmitButton'
 import * as Yup from 'yup'
 import { Form, Formik } from 'formik'
@@ -15,16 +16,15 @@ const Auth = () => {
     identifier: Yup.string().email('invalid email adress').required('email is required'),
     password: Yup.string().min(5, 'password must contains 5 characters').required('password is required')
   })
-  const { login, state: { isLoggedIn } } = useAuth()
+  const { login, state: { isLoggedIn, error } } = useAuth()
   const navigate = useNavigate()
 
   useEffect(() => {
+    console.log(error)
     if (isLoggedIn) {
       navigate('/')
     }
   }, [isLoggedIn])
-
-  const [errorLabel, setErrorLabel] = useState('')
 
   const handleSubmit = (values) => {
     login(values)
@@ -33,7 +33,7 @@ const Auth = () => {
   return (
     <>
       <div className='authForm'>
-        <img className='imgJulo' src={Julo} alt='Jules img' />
+        <img src={error ? juloError : Julo} alt='julo' className='imgJulo' />
         <Formik
           initialValues={{
             identifier: 'toto@tata.fr',
@@ -56,7 +56,7 @@ const Auth = () => {
                 name='password'
                 icon={FaLock}
               />
-              {errorLabel && <div className='error'>{errorLabel}</div>}
+              {error && <p className='error'>{error.message}</p>}
               <SubmitButton label='Connexion' />
             </Form>
           )}
