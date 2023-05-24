d806b0174e39debb5e0a4d29a84c1ccc8e1770f2
Cedbull <66483545+Cedbull@users.noreply.github.com>
2023-05-24 14:31:26 +0200

    Sépare les queries users et posts

--- frontend/src/graphql/queries/postsQueries.js
+++ frontend/src/graphql/queries/postsQueries.js
@@ -47,6 +47,54 @@ query {
 }
 `
 
+const GET_POSTS_BY_USER_ID = (id) => gql`
+query {
+  posts (filters: {user: {id: {eq: ${id} }}}){
+    data {
+      id,
+      attributes {
+        title, 
+        content,
+        likers {
+          data {
+            id
+          }
+        },
+        createdAt, 
+        medias {
+          data {
+            id,
+            attributes {
+              name,
+              url
+            }
+          }
+        },
+        user {
+          data {
+            id,
+            attributes{
+              username,
+              firstName,
+              avatar {
+                data {
+                  id,
+                  attributes {
+                    name,
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+
 export {
-  GET_POSTS
+  GET_POSTS,
+  GET_POSTS_BY_USER_ID
 }
