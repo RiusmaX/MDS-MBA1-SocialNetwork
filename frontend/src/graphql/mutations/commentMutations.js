c1fadfd699f9259155519925f33482ea6fa43805
JulesDrouet <jules.drouet44@gmail.com>
2023-06-07 17:30:56 +0200

    add button to update user post

--- frontend/src/graphql/mutations/commentMutations.js
+++ frontend/src/graphql/mutations/commentMutations.js
@@ -48,6 +48,56 @@ const ADD_COMMENT = gql`mutation CreatePost($content: String!, $userId: ID!, $re
 }
 `
 
+const UPDATE_COMMENT = gql`
+mutation UpdatePost($postId: ID!, $content: String!) {
+  updatePost(    
+    id: $postId,
+    data: {
+    content: $content
+  }) {data {
+      id,
+      attributes {
+        title, 
+        content,
+        likers {
+          data {
+            id
+          }
+        },
+        createdAt, 
+        medias {
+          data {
+            id,
+            attributes {
+              name,
+              url
+            }
+          }
+        },
+        user {
+          data {
+            id,
+            attributes{
+              username,
+              firstName,
+              avatar {
+                data {
+                  id,
+                  attributes {
+                    name,
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }}
+}
+`
+
 export {
-  ADD_COMMENT
+  ADD_COMMENT,
+  UPDATE_COMMENT
 }
