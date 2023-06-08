6932410a0cca5a0fcc8c03c44a3b98624dcd9674
mmeau <m.meau@inlead.fr>
2023-06-08 15:57:39 +0200

    fix: rediretction to profile

--- frontend/src/components/Layout/AppBar.jsx
+++ frontend/src/components/Layout/AppBar.jsx
@@ -72,7 +72,8 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   }
 }))
 
-function ResponsiveAppBar () {
+function ResponsiveAppBar() {
+  const { state: { user } } = useAuth()
   const navigate = useNavigate()
   const [anchorElNav, setAnchorElNav] = React.useState(null)
   const [anchorElUser, setAnchorElUser] = React.useState(null)
@@ -97,12 +98,9 @@ function ResponsiveAppBar () {
   const handleCloseUserMenu = (setting) => {
     // Lors du clique sur le paramètre Profile
     if (setting === 'Profile') {
-      const token = window.localStorage.getItem('token')
-      const decodedToken = jwt_decode(token)
-      const userId = decodedToken.id
       // Redirection sur la page profile de l'utilisateur connecter
       // Modifier lorsque la connexion sera fonctionelle
-      navigate('/users/' + userId)
+      navigate('/users/' + user.id)
     } else if (setting === 'Logout') {
       handleLogout()
     }
