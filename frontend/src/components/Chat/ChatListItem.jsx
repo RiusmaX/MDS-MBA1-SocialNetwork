1b590cf03b0f506f1e8197bc691d7bb765040b86
Killian LE VAILLANT <killian.levaillant@gmail.com>
2023-06-08 14:10:42 +0200

    Add pin icon on chatlist

--- frontend/src/components/Chat/ChatListItem.jsx
+++ frontend/src/components/Chat/ChatListItem.jsx
@@ -4,9 +4,26 @@ import { useQuery } from '@apollo/client'
 import { GET_LAST_CHAT_MESSAGE } from '../../graphql/queries/chatsQueries'
 import { formatDistance } from 'date-fns'
 import { fr } from 'date-fns/locale'
+import { usePin } from '../../contexts/PinMessagesContext'
+import { useEffect, useState } from 'react'
 
-const ChatListItem = ({ chat, onClick, active }) => {
+const ChatListItem = ({ chat, onClick, active, chatId }) => {
   const { loading, error, data } = useQuery(GET_LAST_CHAT_MESSAGE(chat.id))
+  const { state } = usePin()
+  const { pinnedIds } = state
+  const [togglePin, setTogglePin] = useState([])
+
+  useEffect(() => {
+    console.log(pinnedIds)
+    console.log(chatId)
+    if (pinnedIds.length > 0 && pinnedIds.includes(chatId)) {
+      setTogglePin(true)
+      console.log(true)
+    } else {
+      setTogglePin(false)
+      console.log(false)
+    }
+  }, [chatId, pinnedIds])
 
   if (loading) {
     return <h2>Chargement...</h2>
@@ -21,6 +38,7 @@ const ChatListItem = ({ chat, onClick, active }) => {
     )
   }
 
+  console.log(data)
   const sendDate = data?.messages?.data?.[0]?.attributes?.sendDate
   const formattedDistance = sendDate
     ? formatDistance(new Date(sendDate), new Date(), { addSuffix: true, locale: fr })
@@ -44,7 +62,7 @@ const ChatListItem = ({ chat, onClick, active }) => {
             aria-label='recipe'
           />
         }
-        title={chat.attributes.name}
+        title={<span>{togglePin ? '📍' : ''} {chat.attributes.name}</span>}
         subheader={formattedDistance}
       />
     </Card>
